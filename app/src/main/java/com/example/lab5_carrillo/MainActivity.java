b4ffc5e9a34cbeaed22757cf55e6ce5e25ee7bcb
Itzayana Carrillo <carrilloi@g.cofc.edu>
2020-04-07 19:49:07 -0400

    add screenshots

--- app/src/main/java/com/example/lab5_carrillo/MainActivity.java
+++ app/src/main/java/com/example/lab5_carrillo/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.lab5_carrillo;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SharedPreferences mPreferences;
+    private SharedPreferences.Editor mEditor;
+
+
+    private TextView defaultOne;
+    private EditText enterNew;
+    private Button changeButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        defaultOne = findViewById(R.id.userNameTextId);
+        enterNew = findViewById(R.id.enterUserNameId);
+        changeButton = findViewById(R.id.button);
+
+        mPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mPreferences.edit();
+
+        checkPreferences();
+
+        changeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newUserNameText = enterNew.getText().toString().trim();
+
+                if(!newUserNameText.isEmpty()) {
+                    defaultOne.setText(newUserNameText);
+                    mEditor.putString(getString(R.string.enterUserNameId), newUserNameText);
+                    mEditor.commit();
+                }
+
+            }
+        });
+
+
+
     }
+
+    private void checkPreferences(){
+        String enterUserNameId = mPreferences.getString(getString(R.string.enterUserNameId),"defaultName");
+            defaultOne.setText(enterUserNameId);
+
+    }
+
 }
